dd879facc5542e438cacbacaf42fb08e16da69e4
MatRusTy <mathiasp9999@gmail.com>
2022-03-17 18:58:33 +0100

    reformatted main

--- bin/main.ml
+++ bin/main.ml
@@ -2,18 +2,20 @@ open Numbersgame.NumbersGame
 open Numbersgame.Expression
 open Lettersgame.LettersGame
 
+(* NUMBERS GAME *)
 let input = [6; 5; 4; 2; 75; 50]
 let goal = 537
 
 let solution_opt = solve input goal;;
-
-let lg_input = ['s'; 't'; 'p'; 'b'; 'u'; 'a'; 'e'; 'r'; 's']
-let solutions = resolve lg_input 10;;
-
-print_str_list solutions;
 
 if Option.is_none solution_opt then
 	print_endline "No solution exists"
 else
 	(print_endline "Solution found";
 	print_endline (to_string (Option.get solution_opt)))
+
+(* LETTERS GAME *)
+let lg_input = ['s'; 't'; 'p'; 'b'; 'u'; 'a'; 'e'; 'r'; 's']
+let solutions = resolve lg_input 10;;
+
+print_str_list solutions;
